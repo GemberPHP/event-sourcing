a5c81d78d77ed8f1860e56148599aebec9974397
Jeroen de Graaf <gember@jero.work>
2025-10-10 12:42:35 +0200

    Baseline test ignores for PHPStan

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -2,6 +2,12 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$json of function json_decode expects string, mixed given\\.$#',
+	'identifier' => 'argument.type',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Registry/Saga/Cached/CachedSagaRegistryDecorator.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Cannot cast mixed to string\\.$#',
 	'identifier' => 'cast.string',
 	'count' => 2,
@@ -12,6 +18,30 @@ $ignoreErrors[] = [
 	'identifier' => 'argument.type',
 	'count' => 1,
 	'path' => __DIR__ . '/src/Resolver/Common/DomainTag/DomainTagValueHelper.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Gember\\\\EventSourcing\\\\Resolver\\\\Common\\\\SagaId\\\\SagaIdValueHelper\\:\\:getSagaIdValue\\(\\) should return string\\|Stringable but returns mixed\\.$#',
+	'identifier' => 'return.type',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Resolver/Common/SagaId/SagaIdValueHelper.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$sagaEventSubscriberResolver of class Gember\\\\EventSourcing\\\\Registry\\\\Saga\\\\Reflector\\\\ReflectorSagaRegistry constructor expects Gember\\\\EventSourcing\\\\Resolver\\\\Saga\\\\Default\\\\EventSubscriber\\\\SagaEventSubscriberResolver, mixed given\\.$#',
+	'identifier' => 'argument.type',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Registry/Saga/Cached/CachedSagaRegistryDecoratorTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$sagaEventSubscriberResolver of class Gember\\\\EventSourcing\\\\Registry\\\\Saga\\\\Reflector\\\\ReflectorSagaRegistry constructor expects Gember\\\\EventSourcing\\\\Resolver\\\\Saga\\\\Default\\\\EventSubscriber\\\\SagaEventSubscriberResolver, mixed given\\.$#',
+	'identifier' => 'argument.type',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Registry/Saga/Reflector/ReflectorSagaRegistryTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$serializer of class Gember\\\\EventSourcing\\\\Repository\\\\Rdbms\\\\RdbmsSagaStore constructor expects Gember\\\\DependencyContracts\\\\Util\\\\Serialization\\\\Serializer\\\\Serializer, mixed given\\.$#',
+	'identifier' => 'argument.type',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Repository/Rdbms/RdbmsSagaStoreTest.php',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Parameter \\#1 \\$json of function json_decode expects string, mixed given\\.$#',
@@ -32,10 +62,22 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/tests/Resolver/DomainEvent/Cached/CachedDomainEventResolverDecoratorTest.php',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Parameter \\#1 \\$payload of static method Gember\\\\EventSourcing\\\\Resolver\\\\DomainEvent\\\\DomainEventDefinition\\:\\:fromPayload\\(\\) expects array\\{eventClassName\\: class\\-string, eventName\\: string, domainTags\\: list\\<array\\{domainTagName\\: string, type\\: string\\}\\>\\}, mixed given\\.$#',
+	'message' => '#^Parameter \\#1 \\$payload of static method Gember\\\\EventSourcing\\\\Resolver\\\\DomainEvent\\\\DomainEventDefinition\\:\\:fromPayload\\(\\) expects array\\{eventClassName\\: class\\-string, eventName\\: string, domainTags\\: list\\<array\\{domainTagName\\: string, type\\: string\\}\\>, sagaIds\\: list\\<array\\{sagaIdName\\: string, propertyName\\: string\\}\\>\\}, mixed given\\.$#',
 	'identifier' => 'argument.type',
 	'count' => 1,
 	'path' => __DIR__ . '/tests/Resolver/DomainEvent/Cached/CachedDomainEventResolverDecoratorTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$json of function json_decode expects string, mixed given\\.$#',
+	'identifier' => 'argument.type',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Resolver/Saga/Cached/CachedSagaResolverDecoratorTest.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$payload of static method Gember\\\\EventSourcing\\\\Resolver\\\\Saga\\\\SagaDefinition\\:\\:fromPayload\\(\\) expects array\\{sagaClassName\\: class\\-string, sagaName\\: string, sagaId\\: array\\{sagaIdName\\: string, propertyName\\: string\\}, eventSubscribers\\: list\\<array\\{eventClassName\\: class\\-string, methodName\\: string, policy\\: string\\}\\>\\}, mixed given\\.$#',
+	'identifier' => 'argument.type',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/Resolver/Saga/Cached/CachedSagaResolverDecoratorTest.php',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Parameter \\#1 \\$json of function json_decode expects string, mixed given\\.$#',
@@ -54,6 +96,24 @@ $ignoreErrors[] = [
 	'identifier' => 'return.type',
 	'count' => 1,
 	'path' => __DIR__ . '/tests/TestDoubles/Repository/TestUseCaseRepository.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Gember\\\\EventSourcing\\\\Test\\\\TestDoubles\\\\Saga\\\\TestSaga\\:\\:\\$someId \\(string\\) does not accept string\\|null\\.$#',
+	'identifier' => 'assign.propertyType',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/TestDoubles/Saga/TestSaga.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Gember\\\\EventSourcing\\\\Test\\\\TestDoubles\\\\Saga\\\\TestSagaWithNamedInterface\\:\\:\\$someId is unused\\.$#',
+	'identifier' => 'property.unused',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/TestDoubles/Saga/TestSagaWithNamedInterface.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Gember\\\\EventSourcing\\\\Test\\\\TestDoubles\\\\Saga\\\\TestSagaWithoutName\\:\\:\\$someId is unused\\.$#',
+	'identifier' => 'property.unused',
+	'count' => 1,
+	'path' => __DIR__ . '/tests/TestDoubles/Saga/TestSagaWithoutName.php',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Class Gember\\\\EventSourcing\\\\Test\\\\TestDoubles\\\\UseCase\\\\TestFullUseCase has an uninitialized readonly property \\$domainTag\\. Assign it in the constructor\\.$#',
